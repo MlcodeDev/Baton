16d8e55ef1033a2b136d508de1aa75cb4fef48a4
MlcodeDev <mlcode081@gmail.com>
2024-11-29 14:29:36 +0100

    Added precreated color-schemes

---
+++ script.js
@@ -0,0 +1,15 @@
+class ColorScheme {
+  constructor(primary, secondary, font_color, font) {
+    this.primary = primary;
+    this.secondary = secondary;
+    this.font_color = "#FFF";
+    this.font = "Inter";
+  }
+}
+
+const scheme1 = ColorScheme("#C7395F", "#DED4E8", "#E8BA40"),
+  scheme2 = ColorScheme("#80C4B7", "#E3856B", "#E3856B"),
+  scheme3 = ColorScheme("#E87A5D", "#F3B941", "#3B5BA5"),
+  scheme4 = ColorScheme("#E26274", "#F9EC7E", "#E3CCB2"),
+  scheme5 = ColorScheme("#FBEAE7", "#552619", "#B2456E"),
+  scheme6 = ColorScheme("#31473A", "#7C8363", "#EDF4F2");
