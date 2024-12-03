f37cd8b153f252c5b89e6bafbcc12fd21d78126a
MlcodeDev <mlcode081@gmail.com>
2024-12-03 20:51:08 +0100

    Added better schemes

--- script.js
+++ script.js
@@ -42,23 +42,18 @@ document.addEventListener("DOMContentLoaded", () => {
       textElement.style.color = randomScheme.font_color;
     }
   }
-  // Old color schemes
-  // new ColorScheme("#C7395F", "#DED4E8", "#E8BA40");
-  // new ColorScheme("#80C4B7", "#E3856B", "#E3856B");
-  // new ColorScheme("#E87A5D", "#F3B941", "#3B5BA5");
-  // new ColorScheme("#E26274", "#F9EC7E", "#E3CCB2");
-  // new ColorScheme("#FBEAE7", "#552619", "#B2456E");
-  // new ColorScheme("#31473A", "#7C8363", "#EDF4F2");
 
-  new ColorScheme("#dce0d9", "#fbf6ef", "#ead7c3");
   new ColorScheme("#0d3b66", "#faf0ca", "#f4d35e");
-  new ColorScheme("#f6f7eb", "#e94f37", "#393e41");
   new ColorScheme("#006d77", "#83c5be", "#edf6f9");
-  new ColorScheme("#ed6a5a", "#f4f1bb", "#9bc1bc");
   new ColorScheme("#2b2d42", "#8d99ae", "#edf2f4");
-  new ColorScheme("#f4f1de", "#ef476f", "#ffd166");
   new ColorScheme("#064789", "#427aa1", "#ebf2fa");
   new ColorScheme("#0b132b", "#1c2541", "#3a506b");
+  new ColorScheme("#092327", "#0b5351", "#00a9a5");
+  new ColorScheme("#333333", "#48e5c2", "#fcfaf9");
+  new ColorScheme("#cc8b86", "#f9eae1", "#7d4f50");
+  new ColorScheme("#022b3a", "#1f7a8c", "#bfdbf7");
+  new ColorScheme("#283044", "#78a1bb", "#ebf5ee");
+  new ColorScheme("#0a1128", "#001f54", "#034078");
 
   const body = document.querySelector("body");
   const button = document.querySelector(".button-1");
